30632eaa84361cfb26b9bac68b08f921a564492f
OMCS <ols7@aber.ac.uk>
2015-09-26 14:28:18 +0100

    Set true and false as constants

--- src/SAMPi.pl
+++ src/SAMPi.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/env perl
 #
-# SAMPi - SAM4S ECR Reader and Parser (Last Modified 27/09/2015)
+# SAMPi - SAM4S ECR Reader, Parser and Logger (Last Modified 27/09/2015)
 #
 # This software runs as a daemon on a suitably configured Raspberry Pi,
 # reads from a connected SAM4S ECR via RS232, extracts various data
